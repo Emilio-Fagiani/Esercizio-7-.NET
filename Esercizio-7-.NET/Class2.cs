107632f59c92c657f1dfc21d8900e2c158993117
emifa <emifa@LAPTOP-1TM449QB>
2023-06-23 15:59:32 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-7-.NET/Class2.cs
@@ -0,0 +1,27 @@
+﻿public class Class2
+{
+    private int privateVariable;
+    protected int protectedVariable;
+    internal int internalVariable;
+    public int publicVariable;
+
+    private void PrivateMethod()
+    {
+        Console.WriteLine("Private method called.");
+    }
+
+    protected void ProtectedMethod()
+    {
+        Console.WriteLine("Protected method called.");
+    }
+
+    internal void InternalMethod()
+    {
+        Console.WriteLine("Internal method called.");
+    }
+
+    public void PublicMethod()
+    {
+        Console.WriteLine("Public method called.");
+    }
+}
---
+++ Esercizio-7-.NET/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Class
+{
+    static void Main(string[] args)
+    {
+        Class2 class2 = new Class2();
+
+
+        // Accesso ai membri privati
+        //  Class2.privateVariable = 10;
+        //  Class2.PrivateMethod();
+        // 'Class2.privateVariable' non è accessibile a causa del livello di protezione
+        // 'Class2.PrivateMethod()' non è accessibile a causa del livello di protezione
+
+        // Accesso ai membri protetti
+        // Class2.protectedVariable = 20;
+        // Class2.ProtectedMethod();
+        // 'Class2.protectedVariable' non è accessibile a causa del livello di protezione
+        // 'Class2.ProtectedMethod()' non è accessibile a causa del livello di protezione
+
+        // Accesso ai membri interni
+            class2.internalVariable = 30;
+            class2.InternalMethod();
+
+        // Accesso ai membri pubblici
+            class2.publicVariable = 40;
+            class2.PublicMethod();
+    }
+}
+
